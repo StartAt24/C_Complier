61a0377b27d9fae4b85703bc980c701ec8fa0f67
daijunjie <daijunjie@momenta.ai>
2020-10-09 20:02:56 +0800

    add word analyzer

--- ccompiler.c
+++ ccompiler.c
@@ -22,8 +22,74 @@ enum { LEA ,IMM ,JMP ,CALL,JZ  ,JNZ ,ENT ,ADJ ,LEV ,LI  ,LC  ,SI  ,SC  ,PUSH,
        OR  ,XOR ,AND ,EQ  ,NE  ,LT  ,GT  ,LE  ,GE  ,SHL ,SHR ,ADD ,SUB ,MUL ,DIV ,MOD ,
        OPEN,READ,CLOS,PRTF,MALC,MSET,MCMP,EXIT };
 
+// tokens and classes (operators last and in precedence order)
+enum {
+    Num = 128, Fun, Sys, Glo, Loc, Id,
+    Char, Else, Enum, If, Int, Return, Sizeof, While,
+    Assign, Cond, Lor, Lan, Or, Xor, And, Eq, Ne, Lt, Gt, Le, Ge, Shl, Shr, Add, Sub, Mul, Div, Mod, Inc, Dec, Brak
+};
+
+struct identifier{
+    int token; 
+    int hash;       // 标识符的hash值
+    char* name;     // 标识符本身的字符串
+    int class;
+    int type;
+    int value;
+    int Bclass;
+    int Btype;
+    int Bvalue;
+};
+
+int token_val;      // value of current token (mainly for number)
+int *current_id,    // current parsed ID
+    *symbols;       // symbol table
+
+// fields of identifier
+enum {Token, Hash, Name, Type, Class, Value, BType, BClass, BValue, IdSize};
+
+
 void next(){
-    token = *src++;
+    char *last_pos;
+    int hash;
+    while (token = *src){
+        ++src;
+        // parse token here
+
+        if(token == '\n'){
+            ++line;
+        }else if(token == '#'){
+            while(*src !=0 && *src != '\n'){
+                src++;
+            }
+        }else if( (token >= 'a'&& token <= 'z') || (token>'A'&& token<'Z')|| (token == '_')){
+            // parse identifier
+            last_pos = src-1;
+            hash = token;
+
+            while((*src>'a' && *src <= 'z') || (*src>='A' && *src<='Z') || (*src>='0' && *src <='9') || (*src == '_')){
+                hash = hash*147 + *src;
+                src++;
+            }
+
+            // look for existing identifier, linear search
+            current_id = symbols;
+            while( current_id[Token]){
+                if (current_id[Hash] == hash && !memcpy((char*)current_id[Name], last_pos, src-last_pos)){
+                    // found one, return
+                    token = current_id[Token];
+                    return;
+                }
+                current_id = current_id + IdSize;
+            }
+
+            // store new ID
+            current_id[Name] = (int)last_pos;
+            current_id[Hash] = hash;
+            token = current_id[Token] = Id;
+            return;
+        }
+    }
     return;
 }
 
